e4f32f5022f1d824210060f8058649fd76298c38
Jackson Abney <hickorysmokedbacon.yt@gmail.com>
2020-05-25 23:12:47 -0800

    Discord->Minecraft integration is working fully. Minecraft->Discord needs to be done entirely. Also removed some debug logging.

--- src/main/java/hickorysb/forgediscordbridge/DiscordThread.java
+++ src/main/java/hickorysb/forgediscordbridge/DiscordThread.java
@@ -3,14 +3,20 @@ package hickorysb.forgediscordbridge;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.Role;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.gateway.intent.Intent;
 import discord4j.gateway.intent.IntentSet;
 import hickorysb.forgediscordbridge.config.CommandConfig;
 import hickorysb.forgediscordbridge.config.Configuration;
+import hickorysb.forgediscordbridge.config.GroupConfig;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.fml.common.FMLCommonHandler;
+import reactor.core.publisher.Flux;
+
+import java.util.Objects;
 
 public class DiscordThread implements Runnable {
     public static GatewayDiscordClient client;
@@ -19,40 +25,83 @@ public class DiscordThread implements Runnable {
     public void run() {
         if(!Configuration.mainConfig.bot_token.equals("REPLACE_THIS") && !Configuration.mainConfig.bot_token.equals("")) {
             try {
-                client = DiscordClient.create(Configuration.mainConfig.bot_token).gateway().setEnabledIntents(IntentSet.of(Intent.GUILD_MESSAGES)).login().block();
-                ForgeDiscordBridge.logger.info("Logged in as " + client.getSelf().block().getUsername() + ".");
+                client = DiscordClient.create(Configuration.mainConfig.bot_token).gateway().setEnabledIntents(IntentSet.of(Intent.GUILD_MESSAGES, Intent.GUILD_MEMBERS)).login().block();
+                assert client != null : "Client came out null.";
+                ForgeDiscordBridge.logger.info("Logged in as " + Objects.requireNonNull(client.getSelf().block()).getUsername() + ".");
                 client.on(MessageCreateEvent.class).subscribe(event -> {
                     final Message message = event.getMessage();
+                    final Member member = message.getAuthorAsMember().block();
                     final MessageChannel channel = message.getChannel().block();
+                    assert channel != null : "Channel was missing.";
                     if(Configuration.mainConfig.channelIDs.contains(channel.getId().asString())) {
+                        assert message.getAuthor().isPresent() : "Author was missing.";
                         if((Configuration.mainConfig.bridge_bots || !message.getAuthor().get().isBot()) && !message.getContent().startsWith(Configuration.mainConfig.command_prefix)) {
-                            String nickname = message.getAuthorAsMember().block().getNickname().orElse(message.getAuthor().get().getUsername());
+                            assert member != null : "Member was missing.";
+                            String nickname = member.getNickname().orElse(message.getAuthor().get().getUsername());
                             FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerList().sendMessage(new TextComponentString("[" + nickname + "] " + message.getContent()));
                         } else if((Configuration.mainConfig.bridge_bots || !message.getAuthor().get().isBot()) && message.getContent().startsWith(Configuration.mainConfig.command_prefix)) {
-                            ForgeDiscordBridge.logger.info("Received Discord command");
                             CommandConfig command = null;
                             for(CommandConfig c : Configuration.commandsConfig.commands) {
-                                if(c.name.equals(message.getContent().split(" ")[0].replace("/", "")) || c.aliases.contains(message.getContent().split(" ")[0].replace("/", ""))) {
-                                    ForgeDiscordBridge.logger.info("Found command: " + c.name);
+                                if(c.name.equals(message.getContent().split(" ")[0].replace(Configuration.mainConfig.command_prefix, "")) || c.aliases.contains(message.getContent().split(" ")[0].replace(Configuration.mainConfig.command_prefix, ""))) {
                                     command = c;
                                     break;
                                 }
                             }
                             if(command != null) {
-                                ForgeDiscordBridge.logger.info("send command");
-                                FMLCommonHandler.instance().getMinecraftServerInstance().commandManager.executeCommand(new DiscordCommandSender(channel, message.getAuthor().get(), command), command.execute);
+                                boolean canRun = false;
+                                if(Configuration.mainConfig.enable_groups) {
+                                    if(command.groupNames.size() > 0) {
+                                        for(String gn : command.groupNames) {
+                                            for(GroupConfig gc : Configuration.groupsConfig.groups) {
+                                                if(gn.equals(gc.name)) {
+                                                    for(String role : gc.roles) {
+                                                        Flux<Role> c;
+                                                        if(role.startsWith("role:")) {
+                                                            assert member != null;
+                                                            c = member.getRoles().filter(r -> r.getName().equals(role.replace("role:", "")));
+                                                        } else {
+                                                            assert member != null;
+                                                            c = member.getRoles().filter(r -> r.getId().asString().equals(role.replace("id:", "")));
+                                                        }
+                                                        if (Objects.requireNonNull(c.count().block()) > 0) {
+                                                            canRun = true;
+                                                            break;
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                } else {
+                                    for(String role : command.roles) {
+                                        Flux<Role> c;
+                                        if(role.startsWith("role:")) {
+                                            assert member != null;
+                                            c = member.getRoles().filter(r -> r.getName().equals(role.replace("role:", "")));
+                                        } else {
+                                            assert member != null;
+                                            c = member.getRoles().filter(r -> r.getId().asString().equals(role.replace("id:", "")));
+                                        }
+                                        if (Objects.requireNonNull(c.count().block()) > 0) {
+                                            canRun = true;
+                                            break;
+                                        }
+                                    }
+                                }
+                                if (canRun) {
+                                    FMLCommonHandler.instance().getMinecraftServerInstance().commandManager.executeCommand(new DiscordCommandSender(channel, message.getAuthor().get(), command), command.execute);
+                                }
                             }
                         }
                     }
                 });
                 client.onDisconnect().block();
-                ForgeDiscordBridge.logger.info("Exiting");
             } catch(Exception e) {
-                ForgeDiscordBridge.logger.info("Error logging into your bot. This could be caused by:");
-                ForgeDiscordBridge.logger.info("- An invalid token (Most likely)");
-                ForgeDiscordBridge.logger.info("- Discord is down");
-                ForgeDiscordBridge.logger.info("- The server does not have an internet connection");
-                ForgeDiscordBridge.logger.info("- Your bot is banned");
+                ForgeDiscordBridge.logger.error("Error logging into your bot. This could be caused by:");
+                ForgeDiscordBridge.logger.error("- An invalid token (Most likely)");
+                ForgeDiscordBridge.logger.error("- Discord is down");
+                ForgeDiscordBridge.logger.error("- The server does not have an internet connection");
+                ForgeDiscordBridge.logger.error("- Your bot is banned");
             }
         }
     }
